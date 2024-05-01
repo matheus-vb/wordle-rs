3447c1a90d4a8a3321ef7812234dd27fb7b8ae5f
MATHEUS VERAS BATISTA <matheus.vbatista04@gmail.com>
2024-05-01 15:08:17 -0300

    intial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+const GAMES: &str = include_str!("../answers.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let guesser = todo!();
+
+    for answer in GAMES.split_whitespace() {
+        play(answer, guesser);
+    }
+}
+
+fn play<G: Guesser>(answer: &'static str, guesser: G) {}
+
+enum Correctness {
+    //GREEN
+    Correct,
+
+    //YELLOW
+    Misplaced,
+
+    //GRAY
+    Absent,
+}
+
+struct Guess {
+    word: String,
+    maks: [Correctness; 5],
+}
+
+trait Guesser {
+    fn guess(&mut self, history: &[Guess]) -> String;
 }
